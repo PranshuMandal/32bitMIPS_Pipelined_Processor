1940aaf2f2ca63b367553a8f1ce172fadc8c424a
PranshuMandal <142156168+PranshuMandal@users.noreply.github.com>
2023-08-17 22:29:12 +0530

    Update and rename test_mips32.v to test_mips32_2.v

--- test_mips32_2.v
+++ test_mips32_2.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.08.2023 18:36:50
-// Design Name: 
-// Module Name: mips_test1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module test_mips32;
 reg clk1=0, clk2=0;
